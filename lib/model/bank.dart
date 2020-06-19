f8300a595930aa4d49ee7133c87f7d6659934cf8
PoliteCoder97 <politecoder97@gmail.com>
2020-06-19 16:02:05 +0430

    change transaction ui

--- lib/model/bank.dart
+++ lib/model/bank.dart
@@ -10,27 +10,28 @@ class Bank {
   String cardDate;
   Color color;
 
-  Bank(this.name, this.cardNumber,this.amount, this.cardDate, this.color, {this.id});
+  Bank(this.name, this.cardNumber, this.amount, this.cardDate, this.color,
+      {this.id});
 
   static List<Bank> banks = [
-    Bank('Melli', '-',0, '-', Colors.red, id: 1),
-    Bank('Mellat', '-', 0,'-', Colors.pink, id: 2),
-    Bank('Saderat', '-',0, '-', Colors.blue, id: 3),
-    Bank('Mehr Eghtesad', '-',0, '-', Colors.green, id: 4),
-    Bank('Sepah', '-', 0,'-', Colors.blue, id: 5),
-    Bank('Eghtesad Novin', '-', 0,'-', Colors.green, id: 6),
-    Bank('Keshavarzi', '-',0, '-', Colors.green, id: 7),
-    Bank('Tejarat', '-',0, '-', Colors.amber, id: 8),
-    Bank('Refah', '-', 0,'-', Colors.grey, id: 9),
-    Bank('Post Bank', '-',0, '-', Colors.green, id: 10),
-    Bank('Wallet', '-', 0,'-', Colors.brown, id: 11),
+    Bank('Melli', '-', 0, '-', Colors.red[400], id: 1),
+    Bank('Mellat', '-', 0, '-', Colors.pink[400], id: 2),
+    Bank('Saderat', '-', 0, '-', Colors.blue[400], id: 3),
+    Bank('Mehr Eghtesad', '-', 0, '-', Colors.green[400], id: 4),
+    Bank('Sepah', '-', 0, '-', Colors.blue[400], id: 5),
+    Bank('Eghtesad Novin', '-', 0, '-', Colors.green[400], id: 6),
+    Bank('Keshavarzi', '-', 0, '-', Colors.green[400], id: 7),
+    Bank('Tejarat', '-', 0, '-', Colors.amber[400], id: 8),
+    Bank('Refah', '-', 0, '-', Colors.grey[400], id: 9),
+    Bank('Post Bank', '-', 0, '-', Colors.green[400], id: 10),
+    Bank('Wallet', '-', 0, '-', Colors.brown[400], id: 11),
   ];
 
   Map<String, dynamic> toMap() {
     return {
 //      'id': id,
       'name': name,
-      'amount':amount,
+      'amount': amount,
       'cardNumber': cardNumber,
       'cardDate': cardDate,
       'color': color.value.toString(),
